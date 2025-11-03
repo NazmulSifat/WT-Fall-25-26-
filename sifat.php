814d6187bc624ab77e65da6c6e724fa14634f6ec
NazmulSifat <nazmulsifat1788@gmail.com>
2025-11-03 22:43:35 +0600

    Initial

---
+++ sifat.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <center>
+        <h1 style="color: red;"> AIUB REGESTRATION SYSTEM </h1>
+        <hr width="60%;" color="red" size="5">
+
+
+    </center>
+
+
+
+
+
+    <br>
+    <br>
+
+
+
+    First Name <br>
+    <input type="text">
+    <br>
+    <br>
+    Last Name <br>
+    <input type="text">
+    <br>
+    <br>
+
+    Your Age <br>
+    <input type="number">
+
+
+
+    <br>
+    <br>
+    Student ID <br>
+    <input type="number">
+    <br>
+    <br>
+
+
+
+    <label for="cars">Department</label> <br>
+    <select name="cars" id="cars">
+        <option value="cs">CS</option>
+        <option value="EEE">EEE</option>
+        <option value="B.Pharma">B.Pharma</option>
+        <option value="English">English</option>
+    </select>
+    <br>
+    <br>
+    Phone Number <br>
+    <input type="number">
+    <br>
+    <br>
+
+
+    University Email <br>
+    <input type="text">
+    <br>
+    <br>
+
+    Create Password (Must be use atlest 8 carecter) <br>
+    <input type="number">
+    <br>
+    <br>
+
+    Confirm Password <br>
+    <input type="number">
+    <br>
+    <br>
+
+
+
+    Semester <br>
+    <input type="radio"> Spring'24
+    <input type="radio"> Summer'24
+    <input type="radio"> Fall'24
+
+    <br>
+    <br>
+
+
+    Requested Credit (max load 15) <br>
+    <input type="number">
+    <br>
+    <br>
+    <center>
+        <input type="Checkbox"> I Accept All of Tearms and Condition
+    </center>
+    <br>
+    <br>
+    <br>
+    <center>
+
+        <h1 style="color: red;"> Course Selection </h1>
+
+        <hr width="60%;" color="red" size="3">
+
+
+
+    </center>
+
+    <input type="checkbox"> Web Technology <br>
+    <input type="checkbox"> Advance Web Technology <br>
+    <input type="checkbox"> Software Requerment Engineering <br>
+    <input type="checkbox"> Management Information System<br>
+    <input type="checkbox"> Bangladesh And Global Studies <br>
+    <input type="checkbox"> Data Communication <br>
+    <input type="checkbox"> Microprocessor and Embried System <br>
+    <input type="checkbox"> Business Communication <br>
+    <input type="checkbox"> OOP1 <br>
+    <input type="checkbox"> Software Quality And Testing <br>
+
+
+    <br>
+    <br>
+    <br>
+
+
+    Comment / Special Suggestion
+    <input type="text">
+    </div>
+    <br>
+    <br>
+
+
+
+
+
+
+
+
+</body>
+
+</html>
